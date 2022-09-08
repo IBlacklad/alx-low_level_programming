fc4317fb31e673f30b063dbd93476352b96db080
Iyin <iblacklad@gmail.com>
2022-09-08 15:34:37 -0700

    hi

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - PRINTESTESTEST
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	retuen(0);
+
+}
