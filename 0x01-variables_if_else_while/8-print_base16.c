a805181627d2c8dd5d18f2a1033b5dbbaac4d5eb
Iyin <iblacklad@gmail.com>
2022-09-11 16:11:53 -0700

    lkd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Numberz
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '15' ; i++)
+	{
+	putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+
+
+}
