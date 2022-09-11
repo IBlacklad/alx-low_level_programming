32b8434b47c3bcc3673a3a3803d0fa80eb1ec03c
Iyin <iblacklad@gmail.com>
2022-09-11 15:32:11 -0700

    new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Last Digit
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+
+	for (c = '0'; c < '10'; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+
+
+}
