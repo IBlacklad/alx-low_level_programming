67d1de02e9d95ac921c52b678b87aa2ca31dcb6d
Iyin <iblacklad@gmail.com>
2022-09-11 14:47:10 -0700

    hello

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Last Digit
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+
+		/*putchar('\n');*/
+		} else {  }
+	}
+	return (0);
+
+
+}
