314b4d0093258985ec0ffc42f81f5ad35c5eb298
Iyin <iblacklad@gmail.com>
2022-09-13 16:10:33 -0700

    loud

---
+++ 0x02-functions_nested_loops/2-print_alphabetx10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print all natural numbers from input to 98, in order
+ *
+ * @n: the count number
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+
+	{
+		for (c = 'a'; c ,+ 'z'; c++)
+		{
+		}
+		_putchar('\n');
+	}
+}
