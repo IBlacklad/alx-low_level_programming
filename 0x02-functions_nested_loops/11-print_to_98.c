f76846a8f4c81cb71a9621d110c9dde0c4aa9d44
Iyin <iblacklad@gmail.com>
2022-09-13 15:57:50 -0700

    poool

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_alphabet - function to print abc
+ * print_to_98 - print all natural numbers from input to 98, in order
  *
- * Return: 0
+ * @n: the count number
  */
 void print_to_98(int n)
 {
