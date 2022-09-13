86193b58255d429463887aeee21e6bc405c75e1b
Iyin <iblacklad@gmail.com>
2022-09-13 08:12:00 -0700

    lets go

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+I#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Postive or Negative
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
