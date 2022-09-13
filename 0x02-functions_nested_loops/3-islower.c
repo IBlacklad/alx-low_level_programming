f1329d161260efb7d473580be4999056b4bdd4d5
Iyin <iblacklad@gmail.com>
2022-09-13 16:20:01 -0700

    p

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Postive or Negative
+ * Return: Always 0 (success)
+ */
+
+int_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+
+		return(1);
+
+	}
+	else
+		return (0);
+}
