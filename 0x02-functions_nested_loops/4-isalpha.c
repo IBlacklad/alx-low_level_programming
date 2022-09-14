0ce9d37a36e64124529ef3b740fcd6736f6fa169
Iyin <iblacklad@gmail.com>
2022-09-13 17:24:45 -0700

    glad it ended

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _isalpha function to check if c is betetrmain - Postive or Negative
- * @c: is the last 
+ * @c: is the last
  * Return: Always 0 (success)
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && 'c' <= 'z') || (c >= 'A' && c <= 'Z' ))
+	if ((c >= 'a' && 'c' <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
