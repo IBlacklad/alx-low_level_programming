b16c03cef939dc1b7aef3387906e413cc5d1cd40
Iyin <iblacklad@gmail.com>
2022-09-13 17:01:50 -0700

    p

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * print_to_98 - print all natural numbers from input to 98, in order
+ * print_alphabet_x10 - function to print abc 10 times
  *
- * @n: the count number
+ * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	char c, i;
 
-	for (i = 0; i <= 9; i++)
-
+for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
