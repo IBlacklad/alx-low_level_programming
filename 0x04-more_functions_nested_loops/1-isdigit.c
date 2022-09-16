a1a7997ac2572af7d8c52c5bc1f1100398ff6b8c
Iyin <iblacklad@gmail.com>
2022-09-16 16:51:27 -0700

    wow

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <main.h>
+/**
+ * _isdigit - checks if its a digit
+ * @c: character to check
+ * Return: 1 if its a digite therwise 0
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= '0' && c <= '9')
+		return (1);
+return (0);
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 int _isupper(int c);
 
 
-
+int _isdigit(int c);
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
