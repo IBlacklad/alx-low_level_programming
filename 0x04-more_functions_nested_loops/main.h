33f19390daf40a00ccbee1dcaf9d5f2bf96b0123
Iyin <iblacklad@gmail.com>
2022-09-16 17:03:31 -0700

    kk

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,12 +2,15 @@
 #define HOLBERTON_H_INCLUDED
 
 int _isupper(int c);
-
-
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
 int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
+void print_most_numbers(void);
+void more_numbers(void);
+
+
+
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
