bd6c5e94edb5e23688dcbf8b6fe5bb405209f81d
Iyin <iblacklad@gmail.com>
2022-09-21 16:03:47 -0700

    jj

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,20 @@
 char *_strcat(char *dest, char #src)
 {
 
-	 src[50]= "wow its true";
-	dest[50]= "add to";
+	
+	
+
+	for (int a = 0; dest[a] != '\0'; a++)
+	{
+	while (src[b] != '\0')
+	{
+		dest[a] = dest[a] + src[b];
+		b++;
+	}
 
 
-	_strcat(dest, src);
-
-	printf("Destination String is : %s \n", dest);
+	}
+	dest[a] = '\0';
 
 	return (dest);
 
