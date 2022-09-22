1a833635aa8beed1a966e2230dbc371099ce1905
Iyin <iblacklad@gmail.com>
2022-09-22 13:58:28 -0700

    pop

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,18 +3,18 @@
 
 /**
  * _strcat - appends strings
+ * @dest - is a char variable
  * @src - is a variable
- * @dest - is a variable
- * Return: dest
+ * Return: will be void
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int a=0;
-	int b= 0;	
+	int a = 0;
+	int b = 0;
 
 	while (dest[a] != '\0')
 		a++;
-	
 	while (src[b] != '\0')
 	{
 		dest[a] = dest[a] + src[b];
