ce5125b55b05f132f9db25ba02f34f9007aeb8cc
Iyin <iblacklad@gmail.com>
2022-09-22 15:04:24 -0700

    i no goit take your inslult o

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,7 +9,7 @@
  * Return: returns the required output
  */
 
-char *_strncopy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 	for (i = 0; i < n && src[i] != '\0'; i++)
