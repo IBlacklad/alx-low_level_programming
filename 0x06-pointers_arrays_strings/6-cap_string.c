3c5958693abe9f4c85d3a8e90d848425706a6223
Iyin <iblacklad@gmail.com>
2022-09-23 13:31:57 -0700

    Nelo rocks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalize all words in a funto
- * @s: String to be ccapitalized
+ * cap_string - Capitalize all words in a function
+ * @s: String to be capitalized
  *
  * Return: return of s
  */
@@ -10,24 +10,24 @@
 char *cap_string(char *s)
 {
 	int i = 0, j;
-	char a[] " \t\n,;.!?\"()()";
+	char a[] = " \t\n,;.!?\"()()";
 
 	while (*(s + i))
 	{
 		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-		{	
-		if (i == 0)
-			*(s + i) -= 'a' - 'A';
-		else 
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+		else
 		{
 			for (j = 0; j <= 12; j++)
 			{
 			if (a[j] == *(s + i - 1))
-				*(s + i) -= 'a' - 'A';		
+				*(s + i) -= 'a' - 'A';
 			}
 		}
 		}
-		i++'
+		i++;
 	}
 	return (s);
 }
